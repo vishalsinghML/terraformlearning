382df735f3e9ef659e76ed2937cfdf8d4517cc8d
EC2 Default User <ec2-user@ip-172-31-44-241.ap-south-1.compute.internal>
2020-08-11 08:20:56 +0000

    auto-commit 20200811_082056UTC

--- git_pull_terraformlearning.sh
+++ git_pull_terraformlearning.sh
@@ -1,7 +1,12 @@
+echo "++++++++PULLING THE CODE FROM SERVER"
+git pull origin master
 git status
+echo "++++++++ADD THE CHANGES"
 git add *
+echo "++++++++COMMIT THE CHANGES"
 git commit -a -m "auto-commit $(date +%Y%m%d_%H%M%S%Z)"
 git status
+echo "++++++++ PUSH THE CHANGES"
 git push origin master
 git status
 
