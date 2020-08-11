a9bf9332e024957614655081c74e82d394f1ebee
vishal singh <vsingh@live.in>
2020-08-11 14:20:31 +0530

    Update git_pull_terraformlearning.sh

--- git_pull_terraformlearning.sh
+++ git_pull_terraformlearning.sh
@@ -1,10 +1,11 @@
 echo "++++++++PULLING THE CODE FROM SERVER"
 git pull origin master
 git status
+git diff
 echo "++++++++ADD THE CHANGES"
 git add *
 echo "++++++++COMMIT THE CHANGES"
-git commit -a -m "auto-commit $(date +%Y%m%d_%H%M%S%Z)"
+git commit -a -m "auto-commit $(date +%F_%T)"
 git status
 echo "++++++++ PUSH THE CHANGES"
 git push origin master
