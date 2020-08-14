f8846b0950bcb05ff7e742e562c4c9ea8e58b753
Vishal Singh <vishalsingh@arubanetworks.com>
2020-08-14 07:30:50 +0530

    auto-commit 2020-08-14_07:30:50

--- main.tf
+++ main.tf
@@ -2,6 +2,15 @@ provider "aws"{
     region = "ap-south-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket = "mynewbucket14aug"
+    key    = "terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
+
+
 resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
